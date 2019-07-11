5e4d3ea26574a016ae30bbd4464525e7b3735524
phipex <aphipex@gmail.com>
2019-07-11 18:32:28 -0500

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { SorteoBingoService } from './Servicios/sorteo-bingo.service';
-import { Sorteo, ISorteo, EstadoSorteo } from './Modelos/sorteo.model';
+
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
@@ -11,14 +9,9 @@ export class AppComponent implements OnInit {
 
   //private route: ActivatedRoute;
   name = 'Angular';
-  idSala;
-  idSorteo;
-  nombreOperador;
-  sorteo: ISorteo;
+  
   constructor(
-    private route: ActivatedRoute,
-    private router: Router,
-    private sorteoService: SorteoBingoService
+  
   ) { }
 
   ngOnInit() {
